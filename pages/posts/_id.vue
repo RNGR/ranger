75f6d97327073a02137a2bfb1153943fc2cedcb3
Ben Haynes <ben@rngr.org>
2019-09-07 11:53:27 -0400

    update cloud project key

--- pages/posts/_id.vue
+++ pages/posts/_id.vue
@@ -5,7 +5,7 @@
       color="gray" />
     <section class="hero max-width">
       <div class="background-container">
-        <div class="background" :style="{ 'background-image': 'url(https://api.directus.cloud/thumbnail/dcShZiRsguP/1500/400/crop/better/' + hero.filename + ')' }"></div>
+        <div class="background" :style="{ 'background-image': 'url(https://api.directus.cloud/thumbnail/dcdBrg_0chy/1500/400/crop/better/' + hero.filename + ')' }"></div>
       </div>
     </section>
     <section class="post max-width-article">
